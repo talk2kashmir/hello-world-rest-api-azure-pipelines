057b9749049190b47a7b92de34c8820d0ea1db36
Kashmir Singh <kashmir.duley@gmail.com>
2026-09-17 14:46:01 +0530

    v11 fixed pipeline issue

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World 11";
+		return "Hello World 12";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
